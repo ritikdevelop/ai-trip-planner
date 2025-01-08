be38ac5c63de82bb89a2aa194027f236cbef8544
ritikdevelop <hrithikrana216@gmail.com>
2025-01-08 22:30:32 +0530

    Add Colors constants for light and dark themes
    This commit introduces a new file, Colors.ts, which defines color constants for light and dark themes. The Colors object includes properties for text, background, and tint colors, enhancing the project's styling capabilities.

---
+++ constants/Colors.ts
@@ -0,0 +1,24 @@
+const tintColorLight = "#0a7ea4";
+const tintColorDark = "#fff";
+
+export const Colors = {
+  WHITE: "#fff",
+  PRIMARY: "#000",
+  GRAY: '#7d7d7d',
+  light: {
+    text: "#11181C",
+    background: "#fff",
+    tint: tintColorLight,
+    icon: "#687076",
+    tabIconDefault: "#687076",
+    tabIconSelected: tintColorLight,
+  },
+  dark: {
+    text: "#ECEDEE",
+    background: "#000",
+    tint: tintColorDark,
+    icon: "#fff",
+    tabIconDefault: "#fff",
+    tabIconSelected: tintColorDark,
+  },
+};
