74ed3b10ab55b2d986c4b21e40c1e675d8a95661
ritikdevelop <hrithikrana216@gmail.com>
2025-01-09 00:29:52 +0530

    Add font loading and splash screen handling in RootLayout component

--- app/_layout.tsx
+++ app/_layout.tsx
@@ -1,9 +1,33 @@
-import { Stack } from "expo-router";
+import { SplashScreen, Stack } from "expo-router";
+import { useFonts } from "expo-font";
+import { useEffect } from "react";
+import "./global.css";
 
 export default function RootLayout() {
+  const [fontsLoaded] = useFonts({
+    outfit: require("./../assets/fonts/Outfit-Regular.ttf"),
+    "outfit-medium": require("./../assets/fonts/Outfit-Medium.ttf"),
+    "outfit-bold": require("./../assets/fonts/Outfit-Bold.ttf"),
+  });
+
+  useEffect(() => {
+    if (fontsLoaded) {
+      SplashScreen.hideAsync();
+    }
+  }, [fontsLoaded]);
+
+  if (!fontsLoaded) {
+    return null;
+  }
+
   return (
     <Stack>
-      <Stack.Screen name="index" />
+      <Stack.Screen
+        name="index"
+        options={{
+          headerShown: false,
+        }}
+      />
     </Stack>
   );
 }
