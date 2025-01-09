4ac7129826d7b2bbb601f1205de8610a5333e4e2
ritikdevelop <hrithikrana216@gmail.com>
2025-01-09 12:43:47 +0530

    Refactor FirebaseConfig.js to use environment variable for API key and update authentication initialization method

--- configs/FirebaseConfig.js
+++ configs/FirebaseConfig.js
@@ -1,15 +1,11 @@
-// Import the functions you need from the SDKs you need
-import { initializeApp } from "firebase/app";
-import { getAuth } from "firebase/auth";
+import { initializeApp } from 'firebase/app';
+import { initializeAuth } from 'firebase/auth';
+import {  getAuth, getReactNativePersistence } from 'firebase/auth';
+// import ReactNativeAsyncStorage from '@react-native-async-storage/async-storage';
 
 
-// TODO: Add SDKs for Firebase products that you want to use
-// https://firebase.google.com/docs/web/setup#available-libraries
-
-// Your web app's Firebase configuration
-// For Firebase JS SDK v7.20.0 and later, measurementId is optional
 const firebaseConfig = {
-  apiKey: "AIzaSyDp_iIR6C_3N5tgnlRt5aoeCPQ4xd0sHiY",
+  apiKey: process.env.FIREBASE_API_KEY,
   authDomain: "mobile-application-14af2.firebaseapp.com",
   projectId: "mobile-application-14af2",
   storageBucket: "mobile-application-14af2.firebasestorage.app",
@@ -20,5 +16,4 @@ const firebaseConfig = {
 
 //! Initialize Firebase
 export const app = initializeApp(firebaseConfig);
-export const auth = getAuth(app);
-
+export const auth = initializeAuth(app);
