814c0013f9af9da39e15ffed85b1d60b40b0a4b3
ritikdevelop <hrithikrana216@gmail.com>
2025-02-28 02:02:59 +0530

    Add SelectTravelersList and SelectBudgetOptions constants for trip planning

---
+++ constants/Option.js
@@ -0,0 +1,49 @@
+export const SelectTravelersList = [
+    {
+        id:1,
+        title:'Just Me',
+        desc: 'A sole traveles in exploration.',
+        icon: '✈️',
+        people: '1'
+    },
+    {
+        id:2,
+        title:'A Couple',
+        desc: 'Two traveles in tandem',
+        icon: '🥂',
+        people: '2 People'
+    },
+    {
+        id:3,
+        title:'Family',
+        desc: 'A group of fun loving adventurers',
+        icon: '👪',
+        people: '3 to 5 People'
+    },
+    {
+        id:4,
+        title:'Friends',
+        desc: 'A bunch of thrill-seekes looking for adventure',
+        icon: '⛵',
+        people: '5 to 10 People'
+    }
+]
+
+export const SelectBudgetOptions = [
+    {
+        id:1,
+        title:'Cheap',
+        desc: 'Stay conscious of costs',
+        icon: '💸',
+    },{
+        id:2,
+        title:'Moderate',
+        desc: 'Keep cost on the average side',
+        icon: '💸',
+    },{
+        id:3,
+        title:'Luxury',
+        desc: "Don't worry about the cost",
+        icon: '💸',
+    }
+]
